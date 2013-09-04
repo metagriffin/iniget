dd430c33aa33546406501150d690a8025610d2c3
metagriffin <mg.github@uberdev.org>
2013-09-04 14:54:53 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ classifiers = [
 
 setup(
   name                  = 'iniget',
-  version               = '0.1.0',
+  version               = '0.2.0',
   description           = 'A command-line tool to extract values from an iniherit-enabled "INI" file.',
   long_description      = read('README.rst'),
   classifiers           = classifiers,
